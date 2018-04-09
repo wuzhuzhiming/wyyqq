47f8f392cab7eee6d2a9decb5ca98be7709a6932
wuzhuzhiming <ithename@163.com>
2018-04-09 17:27:51 +0800

    添加服务器监听代码

--- qqserver/Frm_server.Designer.cs
+++ qqserver/Frm_server.Designer.cs
@@ -33,81 +33,86 @@
             this.label2 = new System.Windows.Forms.Label();
             this.Tb_port = new System.Windows.Forms.TextBox();
             this.Bt_start = new System.Windows.Forms.Button();
-            this.bt_stop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(131, 84);
+            this.label1.Location = new System.Drawing.Point(98, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "监听IP：";
             // 
             // Tb_ip
             // 
-            this.Tb_ip.Location = new System.Drawing.Point(235, 81);
+            this.Tb_ip.Location = new System.Drawing.Point(176, 65);
+            this.Tb_ip.Margin = new System.Windows.Forms.Padding(2);
             this.Tb_ip.Name = "Tb_ip";
-            this.Tb_ip.Size = new System.Drawing.Size(201, 25);
+            this.Tb_ip.Size = new System.Drawing.Size(152, 21);
             this.Tb_ip.TabIndex = 1;
             this.Tb_ip.Text = "127.0.0.1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(131, 155);
+            this.label2.Location = new System.Drawing.Point(98, 124);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.Size = new System.Drawing.Size(65, 12);
             this.label2.TabIndex = 2;
             this.label2.Text = "监听端口：";
             // 
             // Tb_port
             // 
-            this.Tb_port.Location = new System.Drawing.Point(235, 152);
+            this.Tb_port.Location = new System.Drawing.Point(176, 122);
+            this.Tb_port.Margin = new System.Windows.Forms.Padding(2);
             this.Tb_port.Name = "Tb_port";
-            this.Tb_port.Size = new System.Drawing.Size(201, 25);
+            this.Tb_port.Size = new System.Drawing.Size(152, 21);
             this.Tb_port.TabIndex = 3;
             this.Tb_port.Text = "5000";
             // 
             // Bt_start
             // 
-            this.Bt_start.Location = new System.Drawing.Point(134, 220);
+            this.Bt_start.Location = new System.Drawing.Point(176, 177);
+            this.Bt_start.Margin = new System.Windows.Forms.Padding(2);
             this.Bt_start.Name = "Bt_start";
-            this.Bt_start.Size = new System.Drawing.Size(111, 32);
+            this.Bt_start.Size = new System.Drawing.Size(83, 26);
             this.Bt_start.TabIndex = 4;
             this.Bt_start.Text = "启动服务器";
             this.Bt_start.UseVisualStyleBackColor = true;
             this.Bt_start.Click += new System.EventHandler(this.Bt_start_Click);
             // 
-            // bt_stop
+            // label3
             // 
-            this.bt_stop.Location = new System.Drawing.Point(325, 220);
-            this.bt_stop.Name = "bt_stop";
-            this.bt_stop.Size = new System.Drawing.Size(111, 32);
-            this.bt_stop.TabIndex = 5;
-            this.bt_stop.Text = "关闭服务器";
-            this.bt_stop.UseVisualStyleBackColor = true;
-            this.bt_stop.Click += new System.EventHandler(this.Bt_stop_Click);
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 5;
             // 
             // Frm_server
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(169)))));
-            this.ClientSize = new System.Drawing.Size(582, 353);
-            this.Controls.Add(this.bt_stop);
+            this.ClientSize = new System.Drawing.Size(438, 290);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.Bt_start);
             this.Controls.Add(this.Tb_port);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.Tb_ip);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(600, 400);
-            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.MaximumSize = new System.Drawing.Size(454, 328);
+            this.MinimumSize = new System.Drawing.Size(454, 328);
             this.Name = "Frm_server";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "即时通信-服务器";
+            this.Load += new System.EventHandler(this.Frm_server_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,7 +125,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox Tb_port;
         private System.Windows.Forms.Button Bt_start;
-        private System.Windows.Forms.Button bt_stop;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- qqserver/Frm_server.cs
+++ qqserver/Frm_server.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Net.Sockets;
+using System.Net;
+using System.Threading;
 
 namespace qqserver
 {
@@ -18,16 +20,58 @@ namespace qqserver
         }
 
         //监听套接字
-        static Socket s_watch = null;
+        public static Socket s_listen = null;
+        //监听线程
+        Thread th_listen = null;
 
         //"启动服务器" 按钮的处理
         private void Bt_start_Click(object sender, EventArgs e)
         {
+            //创建监听套接字实例
+            s_listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //设置监听的ip和端口
+            IPAddress addr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint point = new IPEndPoint(addr, int.Parse("5000"));
+            //绑定套接字
+            s_listen.Bind(point);
+            //开始监听，监听队列长度是20
+            s_listen.Listen(20);
+            //创建监听线程
+            th_listen = new Thread(listen_connect);
+            //将线程设置为随着主线程结束而结束
+            th_listen.IsBackground = true;
+            //启动监听线程
+            th_listen.Start();
+            label3.Text = "服务器已启动！";
 
+            //禁用输入框和启动按钮
+            Tb_ip.Enabled = false;
+            Tb_port.Enabled = false;
+            Bt_start.Enabled = false;
         }
 
-        //"关闭服务器" 按钮的处理
-        private void Bt_stop_Click(object sender, EventArgs e)
+        //窗体加载时的处理
+        private void Frm_server_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        //监听客户端连接请求(线程函数)
+        public static void listen_connect()
+        {
+            Socket s_client = null;
+
+            //在循环中监听客户端的连接请求
+            while (true)
+            {
+                //客户端连接成功，并创建一个与客户端进行通信的套接字
+                s_client = s_listen.Accept();
+                //创建一个与客户端进行通信的线程
+            }
+        }
+
+        //接受客户端发送的数据(线程函数)
+        public static void recv_data()
         {
 
         }
