64a1b83e27b5b2663f43cc290df06ea775f98961
wuzhuzhiming <ithename@163.com>
2018-04-09 23:10:05 +0800

    添加数据分发机制

--- qqserver/Frm_server.cs
+++ qqserver/Frm_server.cs
@@ -51,10 +51,10 @@ namespace qqserver
         }
 
         //窗体加载时的处理
-        private void Frm_server_Load(object sender, EventArgs e)
-        {
-            
-        }
+        //private void Frm_server_Load(object sender, EventArgs e)
+        //{
+
+        //}
 
         //监听客户端连接请求(线程函数)
         public static void listen_connect()
@@ -67,11 +67,59 @@ namespace qqserver
                 //客户端连接成功，并创建一个与客户端进行通信的套接字
                 s_client = s_listen.Accept();
                 //创建一个与客户端进行通信的线程
+                ParameterizedThreadStart pthst = new ParameterizedThreadStart(recv_data);
+                Thread th_recv = new Thread(pthst);
+                //将线程设置为随着主线程结束而结束
+                th_recv.IsBackground = true;
+                //启动线程
+                th_recv.Start(s_client);
             }
         }
 
-        //接受客户端发送的数据(线程函数)
-        public static void recv_data()
+        //接受客户端发送的数据(带参数的线程函数)
+        public static void recv_data(object s_clientparam)
+        {
+            Socket s_client = s_clientparam as Socket;
+
+            //在循环中接受客户端发送的数据
+            while(true)
+            {
+                //创建一块接收数据的内存
+                byte[] byte_recv = new byte[1024 * 1024];
+                //接收数据(接收到的数据长度为len)
+                int len = s_client.Receive(byte_recv);
+                //将二进制数据转为字符串
+                string str_recv = Encoding.UTF8.GetString(byte_recv, 0, len);
+                //处理接收到的数据
+                handle_data(s_client, str_recv);
+            }
+        }
+
+        //对接收到的数据，按照类型进行分发处理
+        public static void handle_data(Socket s_client, string str_recv)
+        {
+            if (s_client == null || str_recv == null ||str_recv.Length == 0)
+            {
+                return;
+            }
+
+            //分解接收到的数据(数据已'&'作为分隔符)
+            string[] arr_recv = str_recv.Split('&');
+            if (arr_recv.Length <= 1)
+            {
+                return;
+            }
+
+            //根据数据类型进行分发处理
+            if (arr_recv[0] == "login")
+            {
+                //登陆
+                login(s_client, arr_recv);
+            }
+        }
+
+        //登陆
+        public static void login(Socket s_client, string[] arr_recv)
         {
 
         }
